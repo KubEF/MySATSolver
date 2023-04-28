d21f8fdf5f4c0cdc0d8f789474c003234e299375
Efim Kubishkin <efimkub@mail.ru>
2023-04-28 18:24:47 +0300

    changed measuring and added Program to start my sat

---
+++ SATSolver/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace SATSolver
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var st = new Stopwatch();
+            st.Start();
+            var res = SATSolver.Solve(args[0]);
+            st.Stop();
+            Console.WriteLine("Время выполнения в миллисекундах: ", st.ElapsedMilliseconds);
+            if (res.SatOrNot)
+            {
+                Console.WriteLine(String.Join(" ", res.Solution));
+            }
+            else
+            {
+                Console.Write("UNSAT");
+            }
+        }
+    }
+}
